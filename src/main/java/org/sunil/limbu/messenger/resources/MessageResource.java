34db2ce12cb3856e06ddc9bc2b75b9062fd83017
Sunil Limbu <xxyyxyyxxyyx@gmail.com>
2017-05-19 20:17:06 +0800

    Implemented POST

--- src/main/java/org/sunil/limbu/messenger/resources/MessageResource.java
+++ src/main/java/org/sunil/limbu/messenger/resources/MessageResource.java
@@ -2,8 +2,11 @@ package org.sunil.limbu.messenger.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -13,9 +16,25 @@ import org.sunil.limbu.messenger.service.MessageService;
 @Path("/messages")
 public class MessageResource {
 	MessageService messageService = new MessageService();
+	
 	@GET
-	@Produces(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages(){
 		return messageService.getAllMessages();
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{messageId}")
+	public Message getMessage(@PathParam("messageId") long messageId){
+		return messageService.getMessage(messageId);
+	}
+	
+	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message){
+		return messageService.addMessage(message);
 	}
 }
